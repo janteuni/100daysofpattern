b7860aeba15aab1c7a0ea6076de3d44b4a114e1a
janteuni <janteuni@student.42.fr>
2016-01-10 15:14:54 +0100

    add(day)14

--- pages/Home.js
+++ pages/Home.js
@@ -11,6 +11,7 @@ export default class Home extends React.Component {
 
   render () {
     const days = [
+      { day: 'd014', num:'14', color: 'white' },
       { day: 'd013', num:'13', color: '' },
       { day: 'd012', num:'12', color: '' },
       { day: 'd011', num:'11', color: 'white' },
